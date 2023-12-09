2c44c0ad069fc4d670f507d896316ff9d27ef94b
unknown <mahanteshbpyati@gmail.com>
2023-12-09 01:25:01 -0800

    post Req

--- trackBUddy-api/routes/exercises.js
+++ trackBUddy-api/routes/exercises.js
@@ -8,5 +8,22 @@ router.route('/').get((req,res)=>{
         .catch(err => res.status(400).json('Erro:'+ err));
 });
 
+router.route('/add').post((req,res)=>{
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercises = new Exercises({
+        username,
+        description,
+        duration,
+        date
+    });
+
+    newExercises.save()
+        .then(()=>res.json('Exercise added!'))
+        .catch(err => res.status(400).json('Error:'+err))
+});
 
 module.exports = router;
