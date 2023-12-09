bb4a9ab1bc2a41d21cbbd9cda228646133695bdf
unknown <mahanteshbpyati@gmail.com>
2023-12-09 01:38:56 -0800

    findBy Id

--- trackBUddy-api/routes/exercises.js
+++ trackBUddy-api/routes/exercises.js
@@ -26,4 +26,10 @@ router.route('/add').post((req,res)=>{
         .catch(err => res.status(400).json('Error:'+err))
 });
 
+router.route('/:id').get((req,res)=>{
+    Exercises.findById(req.params.id)
+        .then(exercise => exercise)
+        .catch(err => res.status(400).json('Error:'+err))
+})
+
 module.exports = router;
