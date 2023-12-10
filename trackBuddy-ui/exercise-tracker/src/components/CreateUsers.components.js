90a751e27d319abcf15d6994b875191084fe4d98
unknown <mahanteshbpyati@gmail.com>
2023-12-10 14:58:21 +0530

    toast and add user

--- trackBuddy-ui/exercise-tracker/src/components/CreateUsers.components.js
+++ trackBuddy-ui/exercise-tracker/src/components/CreateUsers.components.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { addUsers } from './services/api';
 
 const  CreateUsers = ()=>{
+    const [userName, setUserName] = useState('')
+    const [age, setage] = useState()
+    const [userAdded,setUserAdded] = useState(false)
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        let exercise = {
+            username: userName,
+            age:age
+        }
+        return addUsers(exercise)
+                .then(data=> data && setUserAdded(true))
+                .catch(err=>console.log(err))
+
+    }
+
+    useEffect(()=>{
+        console.log({userAdded});
+        userAdded &&  toast("User Added!", {
+            position: toast.POSITION.TOP_CENTER,
+            className: 'foo-bar'
+          });
+    },[userAdded])
     return (
-       <div>
-        You are on CreateUsers Page
-       </div>
+        <div className='container ' style={{ width: '40%' }}>
+        {userAdded && < ToastContainer/>}
+        <h3>Create New User </h3>
+        <br />
+        <form
+        onSubmit={(e)=>handleSubmit(e)}
+        >
+            <div className='form-group'>
+                <label>Username : </label>
+                <input type='text'
+                    required
+                    className='form-control'
+                    value={userName}
+                    onChange={e => setUserName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Age</label>
+                <input type='text'
+                    required
+                    className='form-control'
+                    value={age}
+                    onChange={e => setage(e.target.value)}
+                />
+            </div>
+            <br />
+            <div>
+                <button type='submit' className='btn btn-secondary' >Submit</button>
+            </div>
+        </form>
+    </div>
     )
 }
 
--- trackBuddy-ui/exercise-tracker/src/components/services/api.js
+++ trackBuddy-ui/exercise-tracker/src/components/services/api.js
@@ -11,9 +11,14 @@ export const addExercise = async(body) =>{
     let results = await axios.get(`${baseUrl}/users/`)
     return results?.data
  }
- 
+
  export const getExercises = async() =>{
    let results = await axios.get(`${baseUrl}/exercises/`)
    return results?.data
 }
 
+export const addUsers = async(body) =>{
+   let results = await axios.post(`${baseUrl}/users/add`,body)
+   // console.log(results?.data,"ress");
+   return results?.data
+}
